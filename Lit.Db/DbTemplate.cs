5ab8137453678c183bab6ca0774bc24e62054b3d
Lisandro Podesta <lisandro.podesta@gmail.com>
2020-02-21 16:53:55 -0600

    * Created DbTemplate to allow separation of instantiation and execution of templates.

---
+++ Lit.Db/DbTemplate.cs
@@ -0,0 +1,25 @@
+﻿namespace Lit.Db
+{
+    /// <summary>
+    /// Database query/stored procedure template.
+    /// </summary>
+    public class DbTemplate
+    {
+    }
+
+    /// <summary>
+    /// Extension methods for DbTemplate.
+    /// </summary>
+    public static class DbTemplateExtension
+    {
+        /// <summary>
+        /// Executes the template.
+        /// </summary>
+        public static T Exec<T>(this T instance, IDbCommands db)
+            where T : DbTemplate
+        {
+            db.ExecuteTemplate(instance);
+            return instance;
+        }
+    }
+}
