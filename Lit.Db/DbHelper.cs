48d67fb9239152bea06e7659ea10a98187460b6b
Lisandro Podesta <lisandro.podesta@gmail.com>
2020-06-04 17:47:11 -0500

    * Minor change.

--- Lit.Db/DbHelper.cs
+++ Lit.Db/DbHelper.cs
@@ -34,6 +34,8 @@ namespace Lit.Db
                         }
                     }
 
+                    value = value.Replace("\t", "  ");
+
                     query = leftPart + value + rightPart;
                     found = true;
                 }
