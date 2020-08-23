9da420a072f24504d9c049babb9c0f981dfc544f
Lisandro Podesta <lisandro.podesta@gmail.com>
2020-08-22 20:29:35 -0500

    + Added visibility converters to Lit.Ui.Wpf

--- Lit.Common/DataType/TypeHelper.cs
+++ Lit.Common/DataType/TypeHelper.cs
@@ -472,5 +472,39 @@ namespace Lit.DataType
 
             return type.IsSubclassOf(ancestor);
         }
+
+        /// <summary>
+        /// Converts an object to a boolean value.
+        /// </summary>
+        public static bool ToBoolean(object value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            return (Type.GetTypeCode(value.GetType())) switch
+            {
+                TypeCode.Empty => false,
+                TypeCode.Object => true,
+                TypeCode.DBNull => false,
+                TypeCode.Boolean => (bool)value,
+                TypeCode.Char => (char)value != '\0',
+                TypeCode.SByte => (sbyte)value != 0,
+                TypeCode.Byte => (byte)value != 0,
+                TypeCode.Int16 => (short)value != 0,
+                TypeCode.UInt16 => (ushort)value != 0,
+                TypeCode.Int32 => (int)value != 0,
+                TypeCode.UInt32 => (uint)value != 0,
+                TypeCode.Int64 => (long)value != 0,
+                TypeCode.UInt64 => (ulong)value != 0,
+                TypeCode.Single => (float)value != 0,
+                TypeCode.Double => (double)value != 0,
+                TypeCode.Decimal => true,
+                TypeCode.DateTime => true,
+                TypeCode.String => !string.IsNullOrEmpty((string)value),
+                _ => throw new NotImplementedException($"Unknown type [{value.GetType().Name}]"),
+            };
+        }
     }
 }
---
+++ Lit.Ui.Wpf/Converters/VisibleIfZero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using Lit.DataType;
+
+namespace Lit.Ui.Wpf.Converters
+{
+    /// <summary>
+    /// Visible if zero converter.
+    /// </summary>
+    public class VisibleIfZero : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !TypeHelper.ToBoolean(value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
