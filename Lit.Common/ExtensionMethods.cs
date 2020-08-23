3cddee36ca195bf54f905c784f0af5a5120709bb
Lisandro Podesta <lisandro.podesta@gmail.com>
2020-08-22 20:27:33 -0500

    + Extended ExtensionMethods

--- Lit.Common/ExtensionMethods.cs
+++ Lit.Common/ExtensionMethods.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Lit
 {
     public static class ExtensionMethods
     {
+        /// <summary>
+        /// Applies an action for each item in the enumeration.
+        /// </summary>
+        public static void ForEach(this IEnumerable items, Action<object> action)
+        {
+            if (items != null)
+            {
+                foreach (var item in items)
+                {
+                    action(item);
+                }
+            }
+        }
+
         /// <summary>
         /// Applies an action for each item in the enumeration.
         /// </summary>
